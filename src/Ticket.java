2c93cadd27d97426de74c4528856e070dc27a88a
Frederick Rohn <frederickrohn@10-19-91-171.dynapool.wireless.nyu.edu>
2025-04-30 23:48:12 -0400

    create Ticket class

---
+++ src/Ticket.java
@@ -0,0 +1,39 @@
+public class Ticket {
+    private String plate;
+    private String reason;
+    private String dateTime;
+    private String spotId;
+    private boolean resolved;
+
+    public Ticket(String plate, String reason, String dateTime, String spotId) {
+        this.plate = plate;
+        this.reason = reason;
+        this.dateTime = dateTime;
+        this.spotId = spotId;
+        this.resolved = false;
+    }
+
+    public String getPlate() {
+        return plate;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public String getDateTime() {
+        return dateTime;
+    }
+
+    public String getSpotId() {
+        return spotId;
+    }
+
+    public boolean isResolved() {
+        return resolved;
+    }
+
+    public void resolveTicket() {
+        this.resolved = true;
+    }
+}
